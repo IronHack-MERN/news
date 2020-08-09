54410a002348144304ed50832b024e8ded8b7ac0
Elena Piaggio <elenapiaggio@gmail.com>
2020-08-09 16:45:41 +0200

    ADD form component

---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Form = () => {
+    return ( 
+        <div className='row'>
+            <div className='col s12 m8 offset-m2'>
+                <form>
+                    Search news
+                </form>
+            </div>
+        </div>
+     );
+}
+ 
+export default Form;
