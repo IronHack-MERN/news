0e179a8e042fc5bb4340b242624483c3841926e8
Elena Piaggio <elenapiaggio@gmail.com>
2020-08-23 22:42:53 +0200

    implement api connection

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import Header from './components/Header';
-import Form from './components/Form';
+import React, { Fragment, useState, useEffect } from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
 
 function App() {
-
-  const [category, setCategory] = useState('');
+  const [category, setCategory] = useState("");
   const [noticias, guardarNoticias] = useState([]);
 
   useEffect(() => {
     const consultarAPI = async () => {
       const url = `https://newsapi.org/v2/top-headlines?country=de&category=${category}&apiKey=87d631c59e0c4e1698100547578a5ed7`;
-                
+
       const respuesta = await fetch(url);
       const noticias = await respuesta.json();
 
       guardarNoticias(noticias.articles);
       console.log(noticias.articles);
-    }
+    };
     consultarAPI();
   }, [category]);
 
-
   return (
     <Fragment>
-      <Header
-        title= {'News'}
-      />
-      <div className='container white'>
-        <Form
-          setCategory = {setCategory}
-        />
+      <Header title={"News"} />
+      <div className="container white">
+        <Form setCategory={setCategory} />
       </div>
     </Fragment>
   );
