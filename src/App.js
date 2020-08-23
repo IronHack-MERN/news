5e4cf559b68fcd12bc011d57614c8462e92a088c
Elena Piaggio <elenapiaggio@gmail.com>
2020-08-23 21:26:17 +0200

    ADD useEffect for consult to API

--- src/App.js
+++ src/App.js
@@ -5,6 +5,21 @@ import Form from './components/Form';
 function App() {
 
   const [category, setCategory] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=de&category=${category}&apiKey=87d631c59e0c4e1698100547578a5ed7`;
+                
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles);
+      console.log(noticias.articles);
+    }
+    consultarAPI();
+  }, [category]);
+
 
   return (
     <Fragment>
