81124b8a3f8209c3938f0f89e70f4317011015f8
Elena Piaggio <elenapiaggio@gmail.com>
2020-08-23 20:43:26 +0200

    ADD options select

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Form from './components/Form';
 
 function App() {
+
+  const [category, setCategory] = useState('');
+
   return (
     <Fragment>
       <Header
         title= {'News'}
       />
       <div className='container white'>
-        <Form/>
+        <Form
+          setCategory = {setCategory}
+        />
       </div>
     </Fragment>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,44 @@
-import React from 'react';
-import styles from './Form.module.css';
+import React from "react";
+import styles from "./Form.module.css";
+import PropTypes from "prop-types";
+import useSelect from "../hooks/useSelect";
 
-const Form = () => {
-    return ( 
-        <div className={`${styles.search} row`}>
-            <div className='col s12 m8 offset-m2'>
-                <form>
-                    <h2 className={styles.heading}>Search news</h2>
-                    <div className='input-field col s12'>
-                        <input
-                            type='submit'
-                            className={`${styles.btn_block} btn-large amber darken-2`}
-                            value='Search'
-                        />
-                    </div>
-                </form>
-            </div>
-        </div>
-     );
-}
- 
+const Form = ({ setCategory }) => {
+  const OPTIONS = [
+    { value: "general", label: "General" },
+    { value: "business", label: "Business" },
+    { value: "entertainment", label: "Entertainment" },
+    { value: "health", label: "Health" },
+    { value: "science", label: "Science" },
+    { value: "sports", label: "Sports" },
+    { value: "technology", label: "Technology" },
+  ];
+
+  const [category, SelectNews] = useSelect("technology", OPTIONS);
+
+  // submit al form, pasar categoria a app.js
+  const findNews = (e) => {
+    e.preventDefault();
+    setCategory(category);
+  };
+
+  return (
+    <div className={`${styles.search} row`}>
+      <div className="col s12 m8 offset-m2">
+        <form onSubmit={findNews}>
+          <h2 className={styles.heading}>Search news</h2>
+          <SelectNews />
+          <div className="input-field col s12">
+            <input
+              type="submit"
+              className={`${styles.btn_block} btn-large amber darken-2`}
+              value="Search"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
 export default Form;
